c204bbcf2bbe0b180cf4f9faecb9476a19b13739
SuperCoder <supercoder@MacBook-Air-von-SuperCoder-2.local>
2022-03-08 09:21:29 +0100

    first try

---
+++ src/Components/mood.js
@@ -0,0 +1,16 @@
+import { Ghost } from 'react-kawaii';
+import React, { useState } from 'react';
+
+const Mood = () => {
+    const [isHappy, setMood] = useState(true)
+    const mood = () => setMood(!isHappy)
+    return (
+        <div className='ghosty'>
+            <Ghost size={600} mood={isHappy ? "blissful" : "sad"} color="#E0E4E8" />
+            <button onClick={mood}>Change Mood</button>
+        </div>
+    )
+}
+export default Mood
+
+{/* <Ghost size={600} mood="lovestruck" color="#E0E4E8" /> */ }
